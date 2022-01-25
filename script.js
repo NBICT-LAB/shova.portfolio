8f32530b5e45c45aa0a983cea0f034077b7da0d6
MD. ARIFUL ISLAM <arifhstuece@gmail.com>
2022-01-25 20:40:53 +0600

    add img

--- script.js
+++ script.js
@@ -70,4 +70,35 @@ $(document).ready(function(){
             }
         }
     });
+
+    
+        //  gallery
+        $('.gallery > div.bg').each(function(){
+            $(this).wrapAll('<a href="" data-fancybox="gallery"></a>');
+        });
+    
+        $('.gallery a').each(function(){
+            var link = $(this).children('.bg').css('background-image');
+            console.log(link);
+            link = link.replace(/(url\(|\)|")/g,'');
+            $(this).attr('href', link);
+        });
+    
+        $("[data-fancybox]").fancybox({
+            loop: true,
+            buttons: [
+                "zoom",
+                "share",
+                "slideShow",
+                "fullScreen",
+                "download",
+                "thumbs",
+                "close"
+            ]		
+        });
+    
+    
+
+
+
 });
